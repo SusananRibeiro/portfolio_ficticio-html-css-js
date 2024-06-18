cb5ceb3131bbdd372dd96de77b4ec72eac5ba7e5
Susana Ribeiro <118482943+SusananRibeiro@users.noreply.github.com>
2024-06-18 16:31:42 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 // Centraliza as funções de consumo de APIs
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/SusananRibeiro/portfolio_ficticio_v1.0-html-css-js/main/data/profile.json'; // '/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/SusananRibeiro/portfolio_ficticio-html-css-js/main/data/profile.json'; // '/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
